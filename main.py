1ac53e5e81aca0e74c3a2ad3c3f5329796b05b38
lalva224 <112669067+lalva224@users.noreply.github.com>
2024-10-20 16:49:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import utils as ut
 #use other api providers through 1 simple interface
 client = OpenAI(
   base_url = 'https://api.groq.com/openai/v1',
-  api_key = os.environ.get('GROQ_API_KEY')
+  api_key = st.secrets['GROQ_KEY']
 )
 def load_model(filename):
   with open(filename,'rb') as file:
